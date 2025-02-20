8c4a030ba8353a0f408c24a68ea0431b0e69e689
alexyen1000 <alexyen1000@gmail.com>
2025-02-19 21:18:15 -0800

    updating repo ref

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/citysheet/', // Replace with your actual repository name
+  base: '/citysheet_simple/', // Replace with your actual repository name
 });
